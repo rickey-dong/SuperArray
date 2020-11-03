ad0729a4a45e7c09f47bdf12eb107faeebd8c2fe
Rickey Dong <rickeyd2004@gmail.com>
2020-11-02 21:06:11 -0500

    added Mr. K's test case, everything seems to work

--- SuperArrayTester.java
+++ SuperArrayTester.java
@@ -2,26 +2,34 @@ public class SuperArrayTester
 {
   public static void main(String[] args)
   {
-    SuperArray a1 = new SuperArray();
-    System.out.println(a1.size() + " should be 0");
-    a1.add("lol");
-    System.out.println(a1.size() + " should be 1");
-    System.out.println(a1.get(0) + " should be lol");
-    System.out.println(a1.set(0, "LOL") + " should be lol");
-    System.out.println(a1.get(0) + " should be LOL");
-    a1.add("lel");
-    a1.add("lll");
-    a1.add("ooo");
-    a1.add("olo");
-    a1.add("haha");
-    a1.add("hehe");
-    a1.add("heh");
-    a1.add("hah");
-    a1.add("xd");
-    System.out.println(a1.size() + " should be 10");
-    System.out.println(a1.get(9) + " should be xd");
-    a1.add("lul");
-    System.out.println(a1.size() + " should be 11");
-    System.out.println(a1.get(10) + " should be lul");
+    SuperArray laughter = new SuperArray();
+    System.out.println(laughter.size() + " should be 0");
+    laughter.add("lol");
+    System.out.println(laughter.size() + " should be 1");
+    System.out.println(laughter.get(0) + " should be lol");
+    System.out.println(laughter.set(0, "LOL") + " should be lol");
+    System.out.println(laughter.get(0) + " should be LOL");
+    laughter.add("lel");
+    laughter.add("lll");
+    laughter.add("ooo");
+    laughter.add("olo");
+    laughter.add("haha");
+    laughter.add("hehe");
+    laughter.add("heh");
+    laughter.add("hah");
+    laughter.add("xd");
+    System.out.println(laughter.size() + " should be 10");
+    System.out.println(laughter.get(9) + " should be xd");
+    laughter.add("lul");
+    System.out.println(laughter.size() + " should be 11");
+    System.out.println(laughter.get(10) + " should be lul");
+    SuperArray words = new SuperArray();
+    words.add("kani");
+    words.add("uni");
+    words.add("ebi");
+    for(int i = 0; i < words.size(); i++){
+      System.out.println( words.get(i) );
+    }
+    System.out.println("should print:\nkani\nuni\nebi");
   }
 }
